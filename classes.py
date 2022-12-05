344947f307b07882ab86f16a14881823719a8a32
marcusbroman <marcus.broman@skola.taby.se>
2022-12-05 09:31:14 +0100

    version 0.2.2 (new classes)

--- classes.py
+++ classes.py
@@ -1,5 +1,22 @@
 class Player():
-    def __init__(self, name, character, house):
-        self.name = name
+    def __init__(self, playername, character, playerhouse, playerhealth):
+        self.playername = playername
         self.charcter = character
-        self.house = house
+        self.playerhouse = playerhouse
+        self.playerhealth = playerhealth
+
+class weapons():
+    def __init__(self, weaponname):
+        self.weaponname = weaponname
+
+
+class items():
+    def __init__(self, itemname):
+        self.itemname = itemname
+
+
+class enemy():
+    def __init__(self, enemyname, enemyhealth):
+        self.enemyname = enemyname
+        self.enemyhealth = enemyhealth
+        
