335744007c69713e42427e221f814cf422148ee0
Axelo21 <axel.osterberg@skola.taby.se>
2022-12-05 10:56:15 +0100

    Version 0.2.3.1 (Rework of code)

--- functions.py
+++ functions.py
@@ -25,23 +25,7 @@ def MainMenu():
 
         try:
             main_menu_choice = int(input("What Is Your Action? -> "))
-
-            if main_menu_choice == 1:
-                print("Play Placeholder\n")
-                #Play()
-                break
-            elif main_menu_choice == 2:
-                print("How To Play Placeholder\n")
-                HowToPlay()
-            elif main_menu_choice == 3:
-                print("Credits Placeholder\n")
-                ShowCredits()
-            elif main_menu_choice == 4:
-                QuitGame()
-                break
-            else:
-                print("Please Use Numbers To Choose One Of The Options Above\n")
-                time.sleep(2)
+            return main_menu_choice
         except:
             print("Please Use Numbers To Choose One Of The Options Above\n")
             time.sleep(2)
--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from classes import *
 from functions import *
 
-MainMenu()
+while True:
+    main_menu_choice = MainMenu()
+
+    if main_menu_choice == 1:
+        print("Play Placeholder\n")
+        #Play()
+        break
+    elif main_menu_choice == 2:
+        print("How To Play Placeholder\n")
+        HowToPlay()
+    elif main_menu_choice == 3:
+        print("Credits Placeholder\n")
+        ShowCredits()
+    elif main_menu_choice == 4:
+        QuitGame()
+        break
+    else:
+        print("Please Use Numbers To Choose One Of The Options Above\n")
+        time.sleep(2)
