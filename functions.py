981bf4228f5bc58562bf772dade30517055b2542
Axelo21 <axel.osterberg@skola.taby.se>
2022-12-05 08:59:23 +0100

    Verison 0.2

--- functions.py
+++ functions.py
@@ -1,1 +1,33 @@
-print("tjo")
+def QuitGame():
+    print("Game Shutting Down...")
+
+def MainMenu():
+    while True:
+        print("----------------------------------------\n")
+        print("            1.    Play")
+        print("            2. How To Play")
+        print("            3.   Credits")
+        print("            4.    Quit\n")
+        print("----------------------------------------\n")
+
+        try:
+            main_menu_choice = int(input("What Is Your Action? -> "))
+
+            if main_menu_choice == 1:
+                print("Play Placeholder\n")
+                #Play()
+                break
+            elif main_menu_choice == 2:
+                print("How To Play Placeholder\n")
+                #HowToPlay()
+            elif main_menu_choice == 3:
+                print("Credits Placeholder\n")
+                #Credits()
+            elif main_menu_choice == 4:
+                QuitGame()
+                break
+            else:
+                print("Please Use Numbers To Choose One Of The Options Above\n")
+        except:
+            print("Please Use Numbers To Choose One Of The Options Above\n")
+MainMenu()
